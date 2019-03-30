a94f08dfb4c21d41c4e9a8505fb474d7fc42aab3
Saurabh Dhingra <Saurabh Dhingra@192.168.1.5>
2019-03-30 23:07:33 +0530

    My second commit

---
+++ EdurekaProject-30032019/src/day1/BasicProgramming.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class BasicProgramming {
+
+	public static void main(String[] args) {
+
+		int num = 10;
+		
+		int num2 = 20;
+		
+		int sum = num + num2;
+		
+		System.out.println("Total : "+ sum);
+
+	}
+
+}
